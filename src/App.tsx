cc3c16ec56c809245d949ab8024a091562711c91
Q-Mick <quentinmick@protonmail.com>
2023-08-06 13:29:15 -0600

    added expense tracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import Form from "./Components/Form";
+import { useState } from 'react';
+import ExpenseList from './Expense-Tracker/Components/ExpenseList';
 function App() {
+	const [expenses, setExpenses] = useState([
+		{ id: 1, description: 'aaa', amount: 10, category: 'Utilities' },
+		{ id: 2, description: 'bbb', amount: 10, category: 'Utilities' },
+		{ id: 3, description: 'ccc', amount: 10, category: 'Utilities' },
+		{ id: 4, description: 'ddd', amount: 10, category: 'Utilities' },
+	]);
 	return (
 		<div>
-			<Form />
+			<ExpenseList
+				expenses={expenses}
+				onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+			/>
 		</div>
 	);
 }
