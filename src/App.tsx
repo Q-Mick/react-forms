226229ed51ec8e4919766b2965a29f1409652f2c
Q-Mick <quentinmick@protonmail.com>
2023-08-08 10:35:29 -0600

    implemented a try catch block

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import ProductList from './Components/ProductList';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 interface User {
 	id: number;
@@ -12,17 +12,24 @@ function App() {
 	const [users, setUsers] = useState<User[]>([]);
 	const [error, setError] = useState(''); // afterRender
 	useEffect(() => {
-		axios
-			.get<User[]>('https://jsonplaceholder.typicode.com/users')
-			.then(res => setUsers(res.data))
-			.catch(err => setError(err.message));
+		const fetchUsers = async () => {
+			try {
+				const res = await axios.get<User[]>(
+					'https://jsonplaceholder.typicode.com/users'
+				);
+				setUsers(res.data);
+			} catch (err) {
+				setError((err as AxiosError).message);
+			}
+		};
+		fetchUsers();
 	}, []);
 
 	return (
 		<div>
 			{error && <p className="text-danger">{error}</p>}
 			<ul>
-				{users.map(user => (
+				{users.map((user) => (
 					<li key={user.id}>{user.name}</li>
 				))}
 			</ul>
